047210301a238a8a73fd8c85aaaa70a00759836d
korgutlova <korgutlova97@yandex.ru>
2018-09-22 23:57:23 +0300

    Fix bug

--- timur/pgbench/create_extra_function.sql
+++ timur/pgbench/create_extra_function.sql
@@ -5,7 +5,7 @@ declare
   result text := '';
   i integer := 0;
 begin
-  result := random_string(length) || '@' || chars[1+random()*(array_length(chars, 1)-1)]
+  result := random_string(length) || '@' || chars[1+random()*(array_length(chars, 1)-1)];
   return result;
 end;
 $$ language plpgsql;
