02f52c01ce70ffcae2f8d95c9686703fbf627246
Mardanov Timur Rustemovich <timurmardanov97@gmail.com>
2018-09-22 11:07:14 +0300

    fixed

--- timur/reservation.py
+++ timur/reservation.py
@@ -7,8 +7,9 @@ import csv
 import psycopg2
 import hashlib
 from typing import Tuple
+from multiprocessing import Pool, Process, Queue
 
-RESERVATION_ROW_COUNTS = 1000000
+RESERVATION_ROW_COUNTS = 500
 
 
 class JsonSerializable(object):
@@ -31,14 +32,21 @@ class Reservation(JsonSerializable):
     created_at: str
     tickets: Tuple[list, tuple] = []
 
+    def return_md5(self, q, string: str):
+        return q.put(hashlib.md5(string.encode('utf-8')).hexdigest())
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.tickets = list(self.create_tickets(random.randrange(1, 5)))
-        self.created_at = int(time.time())
+        self.created_at = time.time()
         e = map(lambda x: random.choices((string.ascii_lowercase))[0], range(3))
         self.passenger = "{}@{}.{}".format(*e)
-        self.pk = hashlib.md5("{}".format(self.passenger + str(self.created_at)).encode('utf-8')).\
-                hexdigest()
+        q1 = Queue(); q2 = Queue()
+        p1 = Process(target=self.return_md5, args=(q1, self.passenger))
+        p2 = Process(target=self.return_md5, args=(q2, str(self.created_at)))
+        p1.start(); p2.start();
+        p1.join(); p2.join();
+        self.pk = q1.get() + q2.get()
 
     def create_tickets(self, length=1) -> object:
         for i in range(length):
@@ -63,7 +71,7 @@ class Ticket(JsonSerializable):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.place = self.get_random_string(3)
-        self.price = random.uniform(20.0, 100.0)
+        self.price = round(random.uniform(20.0, 100.0), 1)
         self.flight_no = self.get_random_string(4)
         self.type = random.choices(list(self.TYPES.keys()))[0]
         self.user = self.create_user().__dict__
@@ -81,10 +89,23 @@ class User(JsonSerializable):
 
     __domains: tuple = ("hotmail.com", "gmail.com", "aol.com", "mail.com", "mail.kz", "yahoo.com")
 
+
+    def return_fn(self, q):
+        q.put(names.get_first_name())
+
+    def return_ln(self, q):
+        q.put(names.get_last_name())
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.first_name = names.get_first_name()
-        self.last_name = names.get_last_name()
+        q1 = Queue(); q2 = Queue()
+        p1 = Process(target=self.return_fn, args=(q1, ))
+        p2 = Process(target=self.return_ln, args=(q2, ))
+        p1.start(); p2.start();
+        p1.join()
+        self.first_name = q1.get()
+        p2.join()
+        self.last_name = q2.get()
         self.patronymic = ""
         self.email = "{}@{}".format(self.get_random_string(10, case=string.ascii_lowercase),
                                     random.choices(self.domains)[0])
@@ -99,6 +120,7 @@ if __name__ == "__main__":
         fieldnames = ['pk', 'data']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
+        processes = []
         for index in range(RESERVATION_ROW_COUNTS):
             r = Reservation()
             writer.writerow({'pk': r.pk, 'data': r.toJson()})
