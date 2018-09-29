058b3168a5f6dae907b92cbd6701e6e97368f506
Mardanov Timur Rustemovich <timurmardanov97@gmail.com>
2018-09-29 10:06:08 +0300

    Fixed

--- timur/pgbench/insert_pgbench.sql
+++ timur/pgbench/insert_pgbench.sql
@@ -1,1 +1,4 @@
-SELECT * FROM reservation AS rsrv WHERE rsrv.pk LIKE md5(random_string(40)) || '%'
+\set aid random(1000000, 10000000 * :scale)
+BEGIN;
+SELECT * FROM reservation AS rsrv WHERE rsrv.pk LIKE md5(:aid::text) || '%';
+END;
