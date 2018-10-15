178e5fa9eac8cebfd8e7a19db924df635fac2e10
korgutlova <korgutlova@yandex.ru>
2018-10-15 13:09:40 +0300

    Fix bugs

--- counter_app/src/main/java/com/delta/App.java
+++ counter_app/src/main/java/com/delta/App.java
@@ -1,6 +1,8 @@
 package com.delta;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 
 import java.util.ArrayList;
@@ -10,30 +12,50 @@ import java.util.Random;
 
 public class App {
 
-    final static private Random rand = new Random();
+    static private Random rand = new Random();
+    static private Session session;
+    static private Cluster cluster;
+    static private ArrayList<String> setFlights;
+    final static private String insertCQL = "INSERT INTO reservation (passenger, date, tickets) VALUES %s";
+    final static private String updateCQL = "UPDATE counter_reservations SET amount_reservations = amount_reservations + %s WHERE flight_number = '%s'";
+    final static private String exampleRow = "('%s', toTimeStamp(now()), [%s])";
+    final static private String exampleTicket = "{type:'%s', place:'%s', price:%s, flight_number:'%s', passport_data:{email:'%s', first_name:'%s', last_name:'%s', third_name:'%s'}},";
+
+    static {
+        String serverIp = "127.0.0.1";
+        String keyspace = "avia";
+        cluster = Cluster.builder()
+                .addContactPoints(serverIp)
+                .build();
+        session = cluster.connect(keyspace);
+    }
+
+    private static void initFlights() {
+        final String selectCQL = "SELECT flight_number FROM counter_reservations";
+        ResultSet rs = session.execute(selectCQL);
+        setFlights = new ArrayList<String>();
+        for (Row row : rs) {
+            setFlights.add(row.getString("flight_number"));
+        }
+    }
 
     private static HashMap<String, Object> generateData() {
         HashMap<String, Object> customMap = new HashMap<String, Object>();
-        String exampleRow = "('%s', toTimeStamp(now()), [%s])";
-        String exampleTicket = "{type:'%s', place:'%s', price:%s, flight_number:'%s', passport_data:{email:'%s', first_name:'%s', last_name:'%s', third_name:'%s'}},";
         StringBuilder stringBuilder = new StringBuilder();
-        int tickets = rand.nextInt(5);
+        int tickets = rand.nextInt(5) + 1;
         ArrayList<String> flights = new ArrayList<String>();
         String flightNumber;
         for (int i = 0; i < tickets; i++) {
-            flightNumber = getRandomString(5);
+            flightNumber = setFlights.get(rand.nextInt(setFlights.size()));
             flights.add(flightNumber);
             stringBuilder.append(String.format(exampleTicket, getType(), getRandomString(3), getPrice(), flightNumber,
-                     generateEmail(), generateName(), generateName(), generateName()));
+                    generateEmail(), generateName(), generateName(), generateName()));
         }
-        System.out.println(stringBuilder.charAt(stringBuilder.length() - 1));
         stringBuilder.delete(stringBuilder.length() - 1, stringBuilder.length());
-        System.out.println(stringBuilder);
         customMap.put("data", String.format(exampleRow, generateEmail(), String.valueOf(stringBuilder)));
         customMap.put("flight_numbers", flights);
         return customMap;
     }
-
 
     private static String getRandomString(int k) {
         String charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
@@ -67,20 +89,20 @@ public class App {
 
     private static String generateEmail() {
         String charList = "abcdefghijklmnopqrstuvwxyz1234567890";
-        String[] tokens= new String[]{"hotmail.com", "gmail.com", "aol.com", "mail.com", "mail.kz", "yahoo.com", "yandex.ru", "mail.ru"};
-        return String.format("%s@%s", generateWithList(rand.nextInt(30)+ 5, charList), tokens[rand.nextInt(tokens.length)]);
+        String[] tokens = new String[]{"hotmail.com", "gmail.com", "aol.com", "mail.com", "mail.kz", "yahoo.com", "yandex.ru", "mail.ru"};
+        return String.format("%s@%s", generateWithList(rand.nextInt(30) + 5, charList), tokens[rand.nextInt(tokens.length)]);
+    }
+
+    private static void fillCounterReservations(int n) {
+        for (int i = 0; i < n; i++) {
+            session.execute(String.format(updateCQL, 0, getRandomString(5)));
+        }
     }
 
     public static void main(String[] args) {
-        final String serverIp = "127.0.0.1";
-        final String keyspace = "avia";
-        final String insertCQL = "INSERT INTO reservation (passenger, date, tickets) VALUES %s";
-        final String updateCQL = "UPDATE counter_reservations SET amount_reservations = amount_reservations + 1 WHERE flight_number = '%s'";
 
-        Cluster cluster = Cluster.builder()
-                .addContactPoints(serverIp)
-                .build();
-        Session session = cluster.connect(keyspace);
+//        fillCounterReservations(10);
+        initFlights();
         String cqlStatement;
         HashMap<String, Object> maps;
         int n = args.length > 0 ? Integer.parseInt(args[0]) : 1;
@@ -89,10 +111,11 @@ public class App {
             cqlStatement = String.format(insertCQL, maps.get("data"));
             session.execute(cqlStatement);
             for (String flight : (ArrayList<String>) maps.get("flight_numbers")) {
-                cqlStatement = String.format(updateCQL, flight);
+                cqlStatement = String.format(updateCQL, 1, flight);
                 session.execute(cqlStatement);
             }
         }
+
         cluster.close();
     }
 
