e905d77985f1be424856dc5e8a9fd6b81845acdf
korgutlova <korgutlova@yandex.ru>
2018-10-16 01:59:55 +0300

    Add dynamic chart for counter

--- counter_app/src/main/java/com/delta/App.java
+++ counter_app/src/main/java/com/delta/App.java
@@ -114,6 +114,11 @@ public class App {
                 cqlStatement = String.format(updateCQL, 1, flight);
                 session.execute(cqlStatement);
             }
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
         cluster.close();
